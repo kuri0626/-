b30811f35fda78162832d76f2cb4b98e433e177b
kuri0626 <kuri.taka.18@outlook.jp>
2022-12-29 16:09:56 +0900

    ブログ投稿の編集

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html lang="{{str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>blog</title>
+        <title>Blog</title>
         <! -- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel"stylesheet">
     </head>
